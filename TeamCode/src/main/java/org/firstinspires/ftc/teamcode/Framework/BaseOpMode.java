1ee5d230b53e99af265778268826b899c226cd57
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2023-10-12 11:10:29 -0400

    thonk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/BaseOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/BaseOpMode.java
@@ -1,15 +1,16 @@
 package org.firstinspires.ftc.teamcode.Framework;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public abstract class BaseOpMode extends LinearOpMode{
     protected DcMotor[] motors;
     protected Hanger hangers;
-
-// Add IMU
+    protected IMU imu;
 
     protected void initHardware() {
         motors = new DcMotor[]{
@@ -22,9 +23,15 @@ public abstract class BaseOpMode extends LinearOpMode{
         motors[1].setDirection(DcMotorSimple.Direction.REVERSE);
         motors[2].setDirection(DcMotorSimple.Direction.FORWARD);
         motors[3].setDirection(DcMotorSimple.Direction.FORWARD);
-// Remove this
-        Servo claw = hardwareMap.get(Servo.class, "servo");
+
         DcMotor otherMotor = hardwareMap.get(DcMotor.class, "linear_actuator");
         hangers = new Hanger(otherMotor);
+
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        // ADJUST ORIENTATION PARAMETERS TO MATCH THE ROBOT
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
+        imu.initialize(parameters);
     }
 }
