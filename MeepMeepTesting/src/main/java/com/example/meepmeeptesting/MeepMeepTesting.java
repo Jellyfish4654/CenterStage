bbefb1f95ae95b2855a2ead6a1234bfe9031a3b7
Jeffrey <thebaconjr@gmail.com>
2023-11-09 04:54:27 -0500

    Clean Up

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -11,83 +11,95 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(750);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(30, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .build();
-
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(23, -35, 0))
-                .splineToLinearHeading(new Pose2d(43, -35, 0), 0)
-                .build());
-        
-        RoadRunnerBotEntity myLeftmoreBot = new DefaultBotBuilder(meepMeep)
-                .setColorScheme(new ColorSchemeRedDark())
-                .setConstraints(30, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .build();
-    
-        myLeftmoreBot.runAction(myLeftmoreBot.getDrive().actionBuilder(new Pose2d(43, -35, 0))
-                .splineToConstantHeading(new Vector2d(50, -10), 20)
-                .splineToLinearHeading(new Pose2d(60, -10, 0), 0)
-                .build());
-    
-        RoadRunnerBotEntity myLeftlessBot = new DefaultBotBuilder(meepMeep)
-                .setColorScheme(new ColorSchemeRedDark())
-                .setConstraints(30, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .build();
-        
-        myLeftlessBot.runAction(myLeftlessBot.getDrive().actionBuilder(new Pose2d(43, -35, 0))
-                .splineToConstantHeading(new Vector2d(50, -59), 25)
-                .splineToLinearHeading(new Pose2d(60, -59, 0), 0)
-                .build());
-    
-        RoadRunnerBotEntity myLeftKindaNotReallyBot = new DefaultBotBuilder(meepMeep)
-                .setColorScheme(new ColorSchemeRedDark())
-                .setConstraints(30, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .build();
-    
-        myLeftKindaNotReallyBot.runAction(myLeftKindaNotReallyBot.getDrive().actionBuilder(new Pose2d(43, -35, 0))
-                .splineToLinearHeading(new Pose2d(49, -35, 0), 0)
-                .build());
-    
-        RoadRunnerBotEntity myLeftKindaBotYeahWhateverYouWant = new DefaultBotBuilder(meepMeep)
-                .setColorScheme(new ColorSchemeRedDark())
-                .setConstraints(30, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .build();
-    
-        myLeftKindaBotYeahWhateverYouWant.runAction(myLeftKindaBotYeahWhateverYouWant.getDrive().actionBuilder(new Pose2d(43, -35, 0))
-                .splineToLinearHeading(new Pose2d(49, -27, 0), 0)
-                .build());
-    
-        RoadRunnerBotEntity myRightKindaBotYeahWhateverYouWant = new DefaultBotBuilder(meepMeep)
-                .setColorScheme(new ColorSchemeRedDark())
-                .setConstraints(30, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .build();
-    
-        myRightKindaBotYeahWhateverYouWant.runAction(myRightKindaBotYeahWhateverYouWant.getDrive().actionBuilder(new Pose2d(43, -35, 0))
-                .splineToLinearHeading(new Pose2d(49, -43, 0), 0)
-                .build());
-
-        RoadRunnerBotEntity bot1 = new DefaultBotBuilder(meepMeep)
-                // We set this bot to be red
+        RoadRunnerBotEntity RED_PURPLE_LEFT = new DefaultBotBuilder(meepMeep)
                 .setColorScheme(new ColorSchemeBlueDark())
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        bot1.runAction(bot1.getDrive().actionBuilder(new Pose2d(7, -68, Math.toRadians(90)))
-                .splineTo(new Vector2d(17.5, -32), Math.toRadians(90))
+        RED_PURPLE_LEFT.runAction(RED_PURPLE_LEFT.getDrive().actionBuilder(new Pose2d(7, -68, Math.toRadians(90)))
+                .splineToConstantHeading(new Vector2d(7, -38), Math.toRadians(90))
+                .splineToConstantHeading(new Vector2d(0, -32), Math.toRadians(90))
+                .splineToConstantHeading(new Vector2d(7, -38), Math.toRadians(315))
                 .splineToConstantHeading(new Vector2d(24, -36), Math.toRadians(0))
+                .build());
+
+        RoadRunnerBotEntity RED_PURPLE_CENTER = new DefaultBotBuilder(meepMeep)
+                .setColorScheme(new ColorSchemeBlueDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        RED_PURPLE_CENTER.runAction(RED_PURPLE_CENTER.getDrive().actionBuilder(new Pose2d(7, -68, Math.toRadians(90)))
+                .splineToConstantHeading(new Vector2d(7, -48), Math.toRadians(90))
+                .splineToConstantHeading(new Vector2d(17.5, -32), Math.toRadians(90))
+                .splineToConstantHeading(new Vector2d(24, -36), Math.toRadians(0))
+                .build());
+
+        RoadRunnerBotEntity RED_TRAJECTORY_1 = new DefaultBotBuilder(meepMeep)
+                .setConstraints(30, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        RED_TRAJECTORY_1.runAction(RED_TRAJECTORY_1.getDrive().actionBuilder(new Pose2d(23, -35, 0))
+                .splineToLinearHeading(new Pose2d(43, -35, 0), 0)
+                .build());
+
+        RoadRunnerBotEntity RED_YELLOW_LEFT = new DefaultBotBuilder(meepMeep)
+                .setColorScheme(new ColorSchemeBlueDark())
+                .setConstraints(30, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        RED_YELLOW_LEFT.runAction(RED_YELLOW_LEFT.getDrive().actionBuilder(new Pose2d(43, -35, 0))
+                .splineToLinearHeading(new Pose2d(49, -27, 0), 0)
+                .build());
+
+        RoadRunnerBotEntity RED_YELLOW_CENTER = new DefaultBotBuilder(meepMeep)
+                .setColorScheme(new ColorSchemeBlueDark())
+                .setConstraints(30, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        RED_YELLOW_CENTER.runAction(RED_YELLOW_CENTER.getDrive().actionBuilder(new Pose2d(43, -35, 0))
+                .splineToLinearHeading(new Pose2d(49, -35, 0), 0)
+                .build());
+
+        RoadRunnerBotEntity YELLOW_RIGHT = new DefaultBotBuilder(meepMeep)
+                .setColorScheme(new ColorSchemeBlueDark())
+                .setConstraints(30, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        YELLOW_RIGHT.runAction(YELLOW_RIGHT.getDrive().actionBuilder(new Pose2d(43, -35, 0))
+                .splineToLinearHeading(new Pose2d(49, -43, 0), 0)
+                .build());
+
+        RoadRunnerBotEntity PARK_LEFT = new DefaultBotBuilder(meepMeep)
+                .setColorScheme(new ColorSchemeRedDark())
+                .setConstraints(30, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        PARK_LEFT.runAction(PARK_LEFT.getDrive().actionBuilder(new Pose2d(43, -35, 0))
+                .splineToConstantHeading(new Vector2d(50, -10), 20)
+                .splineToLinearHeading(new Pose2d(60, -10, 0), 0)
+                .build());
+
+        RoadRunnerBotEntity RED_PARK_RIGHT = new DefaultBotBuilder(meepMeep)
+                .setColorScheme(new ColorSchemeRedDark())
+                .setConstraints(30, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        RED_PARK_RIGHT.runAction(RED_PARK_RIGHT.getDrive().actionBuilder(new Pose2d(43, -35, 0))
+                .splineToConstantHeading(new Vector2d(50, -59), 25)
+                .splineToLinearHeading(new Pose2d(60, -59, 0), 0)
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_OFFICIAL)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
-                .addEntity(myLeftmoreBot)
-                .addEntity(myLeftlessBot)
-                .addEntity(myLeftKindaNotReallyBot)
-                .addEntity(myLeftKindaBotYeahWhateverYouWant)
-                .addEntity(myRightKindaBotYeahWhateverYouWant)
-                .addEntity(bot1)
+                .addEntity(RED_PURPLE_LEFT)
+                .addEntity(RED_PURPLE_CENTER)
+                .addEntity(RED_TRAJECTORY_1)
+                .addEntity(RED_YELLOW_LEFT)
+                .addEntity(RED_YELLOW_CENTER)
+                .addEntity(YELLOW_RIGHT)
+                .addEntity(PARK_LEFT)
+                .addEntity(RED_PARK_RIGHT)
                 .start();
     }
 }
