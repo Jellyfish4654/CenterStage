b4e6af7d9f59e0f9cb023ac89a723eb797397d1e
matthewlaterza <102398918+matthewlaterza@users.noreply.github.com>
2023-11-25 17:45:22 -0500

    Update AutoAlignment.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/misc/AutoAlignment.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/misc/AutoAlignment.java
@@ -11,7 +11,7 @@ public class AutoAlignment {
     private final IMU imuSensor;
     PIDCoefficients yawCoefficients = new PIDCoefficients(0,0,0);
     private final BasicPID yawController = new BasicPID(yawCoefficients);
-    
+
     public AutoAlignment(DcMotor[] motors, IMU imuSensor) {
         this.motors = motors;
         this.imuSensor = imuSensor;
@@ -20,15 +20,16 @@ public class AutoAlignment {
     public void correctTilt() {
         double yaw = imuSensor.getRobotYawPitchRollAngles().getPitch(AngleUnit.RADIANS);
         double yawCorrection = yawController.calculate(0, yaw);
-        MotorPowers(yawCorrection);
+        rotateWheels(yawCorrection);
     }
 
-    private void MotorPowers(double correction) {
-        int[] powerMap = {1, -1, 1, -1};
-        for (int i = 0; i < motors.length; i++) {
-            double power = correction * powerMap[i];
-            power = Math.max(-1, Math.min(1, power));
-            motors[i].setPower(power);
-        }
+    private void rotateWheels(double correction) {
+        double rightPower = correction;
+        double leftPower = -correction;
+
+        motors[0].setPower(rightPower);
+        motors[1].setPower(rightPower);
+        motors[2].setPower(leftPower);
+        motors[3].setPower(leftPower);
     }
 }
