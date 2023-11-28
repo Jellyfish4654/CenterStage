9edeee3677822a3d6215f0a90a14a04704517a7b
matthewlaterza <102398918+matthewlaterza@users.noreply.github.com>
2023-11-27 23:26:44 -0500

    intake test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/intake.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.tuning;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.Framework.BaseOpMode;
+import org.firstinspires.ftc.teamcode.Framework.misc.AntiTipping;
+import org.firstinspires.ftc.teamcode.Framework.misc.AutoAlignment;
+import org.firstinspires.ftc.teamcode.Framework.misc.ButtonEX;
+import org.firstinspires.ftc.teamcode.Framework.misc.SlewRateLimiter;
+import org.firstinspires.ftc.teamcode.Framework.tubingIntake;
+
+public class intake {
+
+}
