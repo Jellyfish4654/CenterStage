6fce5375ae5e0ed947d0047f9fed2d3138e68944
matthewlaterza <102398918+matthewlaterza@users.noreply.github.com>
2024-01-05 15:14:38 -0500

    Update JellyTele.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JellyTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JellyTele.java
@@ -206,7 +206,9 @@ public class JellyTele extends BaseOpMode {
     }
     protected void setMotorSpeeds(double multiplier, double[] powers) {
         applyPrecisionAndScale(multiplier, powers);
-
+        for(int i = 0; i<powers.length; i++){
+            powers[i] = Math.pow(powers[i],3);
+        }
         int averageTargetPosition = (slides.getTargetPositionLeft() + slides.getTargetPositionRight()) / 2;
         double rate = 1.0;
         if (averageTargetPosition >= 1500) {
