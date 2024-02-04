9b1dd490003fdb2f362c86dfd7c3ac603d57d533
Quackson7 <147100693+Quackson7@users.noreply.github.com>
2024-02-04 18:09:26 -0500

    bad april tags sorry chat
    🍜

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/AprilTagPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/AprilTagPipeline.java
@@ -1,60 +1,57 @@
-//package org.firstinspires.ftc.teamcode.Framework;
-//
-//import android.util.Size;
-//
-//import com.qualcomm.robotcore.hardware.HardwareMap;
-//import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
-//
-//import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-//import org.firstinspires.ftc.vision.VisionPortal;
-//import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-//import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-//import org.openftc.easyopencv.OpenCvCamera;
-//
-//public class AprilTagPipeline {
-//    double x;
-//    double y;
-//
-//    private AprilTagProcessor tagProcessor;
-//    private VisionPortal visionPortal;
-//
-//    public AprilTagPipeline(HardwareMap hardwareMap) {
-//        tagProcessor = new AprilTagProcessor.Builder()
-//                .setDrawAxes(true)
-//                .setDrawCubeProjection(true)
-//                .setDrawTagID(true)
-//                .setDrawTagOutline(true)
-//                .build();
-//
-//        visionPortal = new VisionPortal.Builder()
-//                .addProcessor(tagProcessor)
-//                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
-//                .setCameraResolution(new Size(1920, 1080))
-//                .build();
-//    }
-//
-//    public double getTagX(){
-//        return x;
-//    }
-//
-//    public double getTagY(){
-//        return y;
-//    }
-//
-//    public void update() {
-//        for (AprilTagDetection tag : tagProcessor.getDetections()) {
-//            if (tag.id == 2 || tag.id == 5) {
-//                x = tag.ftcPose.x;
-//                y = tag.ftcPose.y;
-//
-//                telemetry.addData("x", tag.ftcPose.x);
-//                telemetry.addData("y", tag.ftcPose.y);
-//                telemetry.addData("z", tag.ftcPose.z);
-//                telemetry.addData("roll", tag.ftcPose.roll);
-//                telemetry.addData("pitch", tag.ftcPose.pitch);
-//                telemetry.addData("yaw", tag.ftcPose.yaw);
-//                telemetry.update();
-//            }
-//        }
-//    }
-//}
+package org.firstinspires.ftc.teamcode.Framework;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+public class AprilTagPipeline
+{
+	double x;
+	double y;
+
+	private AprilTagProcessor tagProcessor;
+	private VisionPortal visionPortal;
+
+	public AprilTagPipeline(HardwareMap hardwareMap)
+	{
+		tagProcessor = new AprilTagProcessor.Builder()
+				.setDrawAxes(true)
+				.setDrawCubeProjection(true)
+				.setDrawTagID(true)
+				.setDrawTagOutline(true)
+				.build();
+
+		visionPortal = new VisionPortal.Builder()
+				.addProcessor(tagProcessor)
+				.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+				.setCameraResolution(new Size(1920, 1080))
+				.build();
+	}
+
+	public void update()
+	{
+		for (AprilTagDetection tag : tagProcessor.getDetections())
+		{
+			if (tag.id == 2 || tag.id == 5)
+			{
+				x = tag.ftcPose.x;
+				y = tag.ftcPose.y;
+
+				telemetry.addData("x", tag.ftcPose.x);
+				telemetry.addData("y", tag.ftcPose.y);
+				telemetry.addData("z", tag.ftcPose.z);
+				telemetry.addData("roll", tag.ftcPose.roll);
+				telemetry.addData("pitch", tag.ftcPose.pitch);
+				telemetry.addData("yaw", tag.ftcPose.yaw);
+				telemetry.update();
+			}
+		}
+	}
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/BaseOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/BaseOpMode.java
@@ -6,103 +6,118 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
 
 public abstract class BaseOpMode extends LinearOpMode
 {
-    protected DcMotor[] driveMotors;
-    protected DroneLauncher droneServo;
-    protected Intake intakeSystem;
-    protected outtakeServo outakeServos;
-    protected DcMotorEx slideMotorLeft;
-    protected DcMotorEx slideMotorRight;
-    protected DcMotorEx intakeMotor;
-    protected Slides slides;
-    protected CRServo outtakeCRServo;
-    protected outtakeCRServo wheelServo;
-    protected outtakeCRServoAction CRServoAction;
-    protected IMU imuSensor;
-    protected AntiTipping antiTipping;
-    protected AutoAlignment autoAlignment;
+	protected DcMotor[] driveMotors;
+	protected DroneLauncher droneServo;
+	protected Intake intakeSystem;
+	protected outtakeServo outakeServos;
+	protected DcMotorEx slideMotorLeft;
+	protected DcMotorEx slideMotorRight;
+	protected DcMotorEx intakeMotor;
+	protected Slides slides;
+	protected CRServo outtakeCRServo;
+	protected outtakeCRServo wheelServo;
+	protected outtakeCRServoAction CRServoAction;
+	protected IMU imuSensor;
+	protected AntiTipping antiTipping;
+	protected AutoAlignment autoAlignment;
 
-    protected DistanceSensor distanceLeft;
+	protected DistanceSensor distanceLeft;
 
-    protected DistanceSensor distanceRight;
+	protected DistanceSensor distanceRight;
 
-    protected void initHardware()
-    {
-        driveMotors = new DcMotor[]{
-                //control hub
-                hardwareMap.dcMotor.get("motorFR"),
-                hardwareMap.dcMotor.get("motorBR"),
-                hardwareMap.dcMotor.get("motorFL"),
-                hardwareMap.dcMotor.get("motorBL")
-        };
-        setMotorDirections(new DcMotorSimple.Direction[]{
-                DcMotorSimple.Direction.REVERSE, // motorFR
-                DcMotorSimple.Direction.REVERSE, // motorBR
-                DcMotorSimple.Direction.FORWARD, // motorFL
-                DcMotorSimple.Direction.FORWARD  // motorBL
-        });
-        // expansion hub
-        droneServo = new DroneLauncher(hardwareMap.get(Servo.class, "droneServo"));
-        outtakeCRServo = hardwareMap.get(CRServo.class, "wheelServo");
-        outtakeCRServo.setDirection(CRServo.Direction.REVERSE);
-        wheelServo = new outtakeCRServo(outtakeCRServo);
-        CRServoAction = new outtakeCRServoAction(outtakeCRServo);
-        intakeMotor = hardwareMap.get(DcMotorEx.class, "Tubing");
-        intakeMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        intakeMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        intakeMotor.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-        intakeSystem = new Intake(
-                hardwareMap.get(DcMotorEx.class, "Tubing"),
-                hardwareMap.get(Servo.class, "intakeServo")
-        );
+	protected AprilTagPipeline aprilTagPipeline;
 
-        Servo outakeServosLeftServo = hardwareMap.get(Servo.class, "outtakeLeftServo");
-        outakeServosLeftServo.setDirection(Servo.Direction.REVERSE);
-        Servo outakeServosRightServo = hardwareMap.get(Servo.class, "outtakeRightServo");
-        Servo armLeftServo = hardwareMap.get(Servo.class, "armLeftServo");
-        Servo armRightServo = hardwareMap.get(Servo.class, "armRightServo");
-        outakeServos = new outtakeServo(outakeServosLeftServo, outakeServosRightServo, armLeftServo, armRightServo);
+	protected void initHardware()
+	{
+		driveMotors = new DcMotor[]{
+				//control hub
+				hardwareMap.dcMotor.get("motorFR"),
+				hardwareMap.dcMotor.get("motorBR"),
+				hardwareMap.dcMotor.get("motorFL"),
+				hardwareMap.dcMotor.get("motorBL")
+		};
+		setMotorDirections(new DcMotorSimple.Direction[]{
+				DcMotorSimple.Direction.REVERSE, // motorFR
+				DcMotorSimple.Direction.REVERSE, // motorBR
+				DcMotorSimple.Direction.FORWARD, // motorFL
+				DcMotorSimple.Direction.FORWARD  // motorBL
+		});
+		// expansion hub
+		droneServo = new DroneLauncher(hardwareMap.get(Servo.class, "droneServo"));
+		outtakeCRServo = hardwareMap.get(CRServo.class, "wheelServo");
+		outtakeCRServo.setDirection(CRServo.Direction.REVERSE);
+		wheelServo = new outtakeCRServo(outtakeCRServo);
+		CRServoAction = new outtakeCRServoAction(outtakeCRServo);
+		intakeMotor = hardwareMap.get(DcMotorEx.class, "Tubing");
+		intakeMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+		intakeMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+		intakeMotor.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+		intakeSystem = new Intake(
+				hardwareMap.get(DcMotorEx.class, "Tubing"),
+				hardwareMap.get(Servo.class, "intakeServo")
+		);
 
-        slideMotorLeft = hardwareMap.get(DcMotorEx.class, "slideMotorLeft");
-        slideMotorRight = hardwareMap.get(DcMotorEx.class, "slideMotorRight");
-        slideMotorRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        slideMotorRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        slideMotorRight.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        slideMotorRight.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-        slideMotorLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        slideMotorLeft.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        slideMotorLeft.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-        slides = new Slides(slideMotorLeft, slideMotorRight);
-        imuSensor = initializeIMUSensor();
-        antiTipping = new AntiTipping(driveMotors, imuSensor);
-        autoAlignment = new AutoAlignment(driveMotors, imuSensor);
-        distanceLeft = hardwareMap.get(DistanceSensor.class, "distanceLeft");
-        distanceRight = hardwareMap.get(DistanceSensor.class, "distanceRight");
+		Servo outakeServosLeftServo = hardwareMap.get(Servo.class, "outtakeLeftServo");
+		outakeServosLeftServo.setDirection(Servo.Direction.REVERSE);
+		Servo outakeServosRightServo = hardwareMap.get(Servo.class, "outtakeRightServo");
+		Servo armLeftServo = hardwareMap.get(Servo.class, "armLeftServo");
+		Servo armRightServo = hardwareMap.get(Servo.class, "armRightServo");
+		outakeServos = new outtakeServo(outakeServosLeftServo, outakeServosRightServo, armLeftServo, armRightServo);
 
-    }
+		slideMotorLeft = hardwareMap.get(DcMotorEx.class, "slideMotorLeft");
+		slideMotorRight = hardwareMap.get(DcMotorEx.class, "slideMotorRight");
+		slideMotorRight.setDirection(DcMotorSimple.Direction.REVERSE);
+		slideMotorRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+		slideMotorRight.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+		slideMotorRight.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+		slideMotorLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+		slideMotorLeft.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+		slideMotorLeft.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+		slides = new Slides(slideMotorLeft, slideMotorRight);
+		imuSensor = initializeIMUSensor();
+		antiTipping = new AntiTipping(driveMotors, imuSensor);
+		autoAlignment = new AutoAlignment(driveMotors, imuSensor);
+		distanceLeft = hardwareMap.get(DistanceSensor.class, "distanceLeft");
+		distanceRight = hardwareMap.get(DistanceSensor.class, "distanceRight");
 
-    private IMU initializeIMUSensor()
-    {
-        IMU imu = hardwareMap.get(IMU.class, "imu");
+        runAprilTagPipeline();
+	}
 
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD,
-                RevHubOrientationOnRobot.UsbFacingDirection.UP
-        ));
-        imu.initialize(parameters);
-        return imu;
-    }
+	private IMU initializeIMUSensor()
+	{
+		IMU imu = hardwareMap.get(IMU.class, "imu");
 
-    private void setMotorDirections(DcMotorSimple.Direction[] directions)
-    {
-        for (int i = 0; i < driveMotors.length; i++)
-        {
-            driveMotors[i].setDirection(directions[i]);
-        }
-    }
+		IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+				RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD,
+				RevHubOrientationOnRobot.UsbFacingDirection.UP
+		));
+		imu.initialize(parameters);
+		return imu;
+	}
+
+	private void setMotorDirections(DcMotorSimple.Direction[] directions)
+	{
+		for (int i = 0; i < driveMotors.length; i++)
+		{
+			driveMotors[i].setDirection(directions[i]);
+		}
+	}
+
+	private void runAprilTagPipeline()
+	{
+		if (opModeIsActive())
+		{
+			while (opModeIsActive())
+            {
+                aprilTagPipeline.update();
+                sleep(20);
+			}
+		}
+	}
 }
