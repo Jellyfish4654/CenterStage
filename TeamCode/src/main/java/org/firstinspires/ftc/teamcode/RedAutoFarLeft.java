3ecbf092de66035313d9cdd6bdd26136db790f5d
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2024-02-08 17:21:23 -0500

    Red Purpl

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutoFarLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutoFarLeft.java
@@ -1,12 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.Framework.BaseOpMode;
+import org.firstinspires.ftc.teamcode.Framework.misc.ActionStorage;
 import org.firstinspires.ftc.teamcode.Framework.misc.RedPipeline;
 import org.firstinspires.ftc.teamcode.Framework.misc.Sides;
 import org.firstinspires.ftc.teamcode.RoadRunner.MecanumDrive;
@@ -28,6 +32,7 @@ public class RedAutoFarLeft extends BaseOpMode
     public void runOpMode()
     {
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(15, -60, Math.toRadians(90)));
+
         Sides.setColor(Sides.Color.RED);
         // Initialize hardware and pipeline
         initHardware(hardwareMap);
@@ -49,93 +54,31 @@ public class RedAutoFarLeft extends BaseOpMode
         drive.pose = new Pose2d(distance, -60, Math.toRadians(90));
         // After starting, stop the camera stream
         webcam.stopStreaming();
-
+        ActionStorage actionStorage = new ActionStorage(drive);
         // Run the autonomous path based on the detected position
+        Action leftPurple = actionStorage.getRedFarRight_LeftPurpleAction();
+        Action centerPurple = actionStorage.getRedFarRight_CenterPurpleAction();
+        Action rightPurple = actionStorage.getRedFarRight_RightPurpleAction();
         switch (detectedPosition)
         {
             case LEFT:
-//                Actions.runBlocking(new SequentialAction(
-                // Blue Left Purple Left
-                drive.actionBuilder(new Pose2d(13, -60, Math.toRadians(90)))
-                        .splineToConstantHeading(new Vector2d(13, -48), Math.toRadians(90))
-                        .splineTo(new Vector2d(7.5, -36.5), Math.toRadians(135))
-                        .build();
-//                ),
-//
-//                        (telemetryPacket) -> {
-//                            sleep(9000);
-//                            return false; // Returning true causes the action to run again, returning false causes it to cease
-//                        },
-//                                //park
-//                        new SequentialAction(
-                drive.actionBuilder(new Pose2d(7.5, -36.5, Math.toRadians(135)))
-                        .strafeTo(new Vector2d(9.7, -39.3))
-                        .turn(Math.toRadians(-45))
-                        .strafeTo(new Vector2d(9.7, -58.9))
-                        .strafeTo(new Vector2d(107, -58))
-                        .build();
-//                                ,
-//                                new ParallelAction(
-//                                        telemetryPacket -> {
-//                                            slides.setTargetPosition(1775);
-//                                            return slides.slideCheck();
-//                                        },
-//                                        telemetryPacket -> {
-//                                            slides.update();
-//                                            return slides.slideCheck();
-//                                        }
-//                                )
-                // Red Right Yellow Left
-//                                    drive.actionBuilder(new Pose2d(40, -36, Math.toRadians(0)))
-//                                            .splineToConstantHeading(new Vector2d(44.4, -28), Math.toRadians(0))
-//                                            .splineToLinearHeading(new Pose2d(49, -28, 0), 0)
-//                                            .splineToLinearHeading(new Pose2d(46.3, -28, 0), Math.toRadians(90))
-//                                            .splineToLinearHeading(new Pose2d(46.3, -12, 0), Math.toRadians(90))
-//                                            .build(),
-//                                    // Red Right Park
-//                                    drive.actionBuilder(new Pose2d(46.3, -12, Math.toRadians(0)))
-//                                            .splineToConstantHeading(new Vector2d(59, -12), Math.toRadians(0))
-//                                            .build()
+                Actions.runBlocking(new SequentialAction(
+                               leftPurple
+                        )
+                );
                 break;
             case CENTER:
-//                Actions.runBlocking(new SequentialAction(
-                // Red Right Purple Middle
-                drive.actionBuilder(new Pose2d(13, -60, Math.toRadians(90)))
-                        .splineTo(new Vector2d(13, -32), Math.toRadians(90))
-                        .build();
-
-//                        new Action() {
-//                            @Override
-//                            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-//                                sleep(9000);
-//                                return false;
-//                            }
-//                        },
-                //park
-                drive.actionBuilder(new Pose2d(13, -60, Math.toRadians(90)))
-                        .strafeTo(new Vector2d(13, -60))
-                        .strafeTo(new Vector2d(105, -58))
-                        .build();
+                Actions.runBlocking(new SequentialAction(
+                                centerPurple
+                        )
+                );
                 break;
             case RIGHT:
             case UNKNOWN:
-//                Actions.runBlocking(new SequentialAction(
-                // Red Right Purple Right
-                drive.actionBuilder(new Pose2d(13, -60, Math.toRadians(90)))
-                        .splineTo(new Vector2d(13, -46), Math.toRadians(90))
-                        .splineTo(new Vector2d(18, -38), Math.toRadians(60))
-                        .build();
-//                        (telemetryPacket) -> {
-//                            sleep(9000);
-//                            return false; // Returning true causes the action to run again, returning false causes it to cease
-//                        },
-                //park
-                drive.actionBuilder(new Pose2d(18, -38, Math.toRadians(60)))
-                        .splineToConstantHeading(new Vector2d(13, -46), Math.toRadians(60))
-                        .turn(Math.toRadians(30))
-                        .strafeTo(new Vector2d(9.7, -58.9))
-                        .strafeTo(new Vector2d(107, -58))
-                        .build();
+                Actions.runBlocking(new SequentialAction(
+                                rightPurple
+                        )
+                );
                 break;
         }
     }
