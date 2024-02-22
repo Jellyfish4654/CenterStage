9445e6548e7ab75ebb5031e2839c485b73689af5
Jeffrey <thebaconjr@gmail.com>
2024-02-22 12:27:34 -0500

    mb

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/AutoAlignment.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/AutoAlignment.java
@@ -3,66 +3,67 @@ package org.firstinspires.ftc.teamcode.Framework;
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
-import org.firstinspires.ftc.teamcode.JellyTele;
+import org.firstinspires.ftc.teamcode.Framework.Profiles.MotionProfile;
+import org.firstinspires.ftc.teamcode.Framework.Profiles.MotionProfileGenerator;
+import org.firstinspires.ftc.teamcode.Framework.Profiles.MotionState;
 
-public class AutoAlignment
-{
-	private final DcMotor[] motors;
-	private final IMU imuSensor;
-	private final PIDController pidController;
+public class AutoAlignment {
+	private DcMotor[] motors;
+	private IMU imuSensor;
+	private PIDController pidController;
+	private MotionProfile profile;
+	private ElapsedTime timer;
 	private double targetAngle = 0;
-	public static double kP = 0.005;
+	public static double kP = 0.02;
 	public static double kI = 0.0;
 	public static double kD = 0;
+	private double maxAngularVelocity = 90;
+	private double maxAngularAcceleration = 180;
 
-	// Constructor
-	public AutoAlignment(DcMotor[] motors, IMU imuSensor)
-	{
+	public AutoAlignment(DcMotor[] motors, IMU imuSensor) {
 		this.motors = motors;
 		this.imuSensor = imuSensor;
 		this.pidController = new PIDController(kP, kI, kD);
+		this.timer = new ElapsedTime();
 	}
 
-	public void setTargetAngle(double targetAngle)
-	{
-		this.targetAngle = targetAngle;
+	public void setTargetAngle(double TargetAngle) {
+		targetAngle = TargetAngle;
+		double currentAngle = getCurrentAngle();
+		MotionState startState = new MotionState(currentAngle, 0);
+		MotionState endState = new MotionState(targetAngle, 0);
+		this.profile = MotionProfileGenerator.generateSimpleMotionProfile(startState, endState, maxAngularVelocity, maxAngularAcceleration);
+		timer.reset();
 	}
 
-	public void update()
-	{
+	public void update() {
+		double elapsedTime = timer.seconds();
+		MotionState targetState = profile.get(elapsedTime);
+		double currentAngle = getCurrentAngle();
+		double correction = calculateMotorPower(currentAngle, targetState);
+		applyMotorPowers(correction);
+	}
+
+	private double getCurrentAngle() {
 		YawPitchRollAngles orientation = imuSensor.getRobotYawPitchRollAngles();
-		double currentYaw = orientation.getYaw(AngleUnit.DEGREES);
-		currentYaw = normalizeAngle(currentYaw);
-		double angleDifference = normalizeAngle(targetAngle - currentYaw);
-		double correction = pidController.calculate(0, angleDifference);
-		MotorPowers(correction);
+		return orientation.getYaw(AngleUnit.DEGREES);
 	}
 
-	private void MotorPowers(double correction)
-	{
-		double rightPower = -correction;
-		double leftPower = correction;
+	private double calculateMotorPower(double currentAngle, MotionState targetState) {
+		return pidController.calculate(currentAngle, targetState.getX());
+	}
+
+	private void applyMotorPowers(double correction) {
+		double rightPower = correction;
+		double leftPower = -correction;
 
 		motors[0].setPower(leftPower);
 		motors[1].setPower(leftPower);
 		motors[2].setPower(rightPower);
 		motors[3].setPower(rightPower);
-	}
-
-	private double normalizeAngle(double angle)
-	{
-		angle = angle % 360;
-		if (angle <= -180)
-		{
-			angle += 360;
-		}
-		else if (angle > 180)
-		{
-			angle -= 360;
-		}
-		return angle;
 	}
 }
