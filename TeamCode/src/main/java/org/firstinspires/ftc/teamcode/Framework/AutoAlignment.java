156cc6174898f89f4b41b8b75aab098611dc7c48
Jeffrey <thebaconjr@gmail.com>
2024-02-24 13:18:21 -0500

    aa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/AutoAlignment.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/AutoAlignment.java
@@ -1,69 +1,75 @@
 package org.firstinspires.ftc.teamcode.Framework;
 
 import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.trajectory.TrapezoidProfile;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
-import org.firstinspires.ftc.teamcode.Framework.Profiles.MotionProfile;
-import org.firstinspires.ftc.teamcode.Framework.Profiles.MotionProfileGenerator;
-import org.firstinspires.ftc.teamcode.Framework.Profiles.MotionState;
 
 public class AutoAlignment {
-	private DcMotor[] motors;
-	private IMU imuSensor;
-	private PIDController pidController;
-	private MotionProfile profile;
-	private ElapsedTime timer;
+	private final DcMotor[] motors;
+	private final IMU imuSensor;
+	private final PIDController pidController;
 	private double targetAngle = 0;
-	public static double kP = 0.02;
+	public static double kP = 0.04;
 	public static double kI = 0.0;
-	public static double kD = 0;
-	private double maxAngularVelocity = 90;
-	private double maxAngularAcceleration = 180;
+	public static double kD = 0.0004;
+	private TrapezoidProfile trapezoidProfile;
+	private TrapezoidProfile.Constraints profileConstraints;
+	private ElapsedTime timer;
+	double maxAngularVelocity = 180;
+	double maxAngularAcceleration = 180;
 
+
+	// Constructor
 	public AutoAlignment(DcMotor[] motors, IMU imuSensor) {
 		this.motors = motors;
 		this.imuSensor = imuSensor;
 		this.pidController = new PIDController(kP, kI, kD);
+		this.profileConstraints = new TrapezoidProfile.Constraints(maxAngularVelocity, maxAngularAcceleration);
+		this.trapezoidProfile = new TrapezoidProfile(profileConstraints, new TrapezoidProfile.State(0, 0));
 		this.timer = new ElapsedTime();
+		this.timer.reset();
 	}
 
-	public void setTargetAngle(double TargetAngle) {
-		targetAngle = TargetAngle;
-		double currentAngle = getCurrentAngle();
-		MotionState startState = new MotionState(currentAngle, 0);
-		MotionState endState = new MotionState(targetAngle, 0);
-		this.profile = MotionProfileGenerator.generateSimpleMotionProfile(startState, endState, maxAngularVelocity, maxAngularAcceleration);
+	public void setTargetAngle(double targetAngle) {
+		this.targetAngle = targetAngle;
+		this.trapezoidProfile = new TrapezoidProfile(profileConstraints, new TrapezoidProfile.State(targetAngle, 0), trapezoidProfile.calculate(timer.seconds()));
 		timer.reset();
 	}
 
 	public void update() {
-		double elapsedTime = timer.seconds();
-		MotionState targetState = profile.get(elapsedTime);
-		double currentAngle = getCurrentAngle();
-		double correction = calculateMotorPower(currentAngle, targetState);
-		applyMotorPowers(correction);
-	}
-
-	private double getCurrentAngle() {
 		YawPitchRollAngles orientation = imuSensor.getRobotYawPitchRollAngles();
-		return orientation.getYaw(AngleUnit.DEGREES);
+		double currentYaw = normalizeAngle(orientation.getYaw(AngleUnit.DEGREES));
+		double angleDifference = normalizeAngle(targetAngle - currentYaw);
+		double elapsedTime = timer.seconds();
+		TrapezoidProfile.State currentGoal = new TrapezoidProfile.State(angleDifference, 0);
+		this.trapezoidProfile = new TrapezoidProfile(this.profileConstraints, currentGoal, new TrapezoidProfile.State(0, trapezoidProfile.calculate(elapsedTime).velocity));
+		TrapezoidProfile.State desiredState = trapezoidProfile.calculate(elapsedTime);
+		double correction = pidController.calculate(currentYaw, desiredState.position);
+		MotorPowers(correction);
 	}
 
-	private double calculateMotorPower(double currentAngle, MotionState targetState) {
-		return pidController.calculate(currentAngle, targetState.getX());
-	}
 
-	private void applyMotorPowers(double correction) {
+	private void MotorPowers(double correction) {
 		double rightPower = correction;
 		double leftPower = -correction;
 
-		motors[0].setPower(leftPower);
-		motors[1].setPower(leftPower);
-		motors[2].setPower(rightPower);
-		motors[3].setPower(rightPower);
+		motors[0].setPower(rightPower);
+		motors[1].setPower(rightPower);
+		motors[2].setPower(leftPower);
+		motors[3].setPower(leftPower);
+	}
+
+	private double normalizeAngle(double angle) {
+		angle = angle % 360;
+		if (angle <= -180) {
+			angle += 360;
+		} else if (angle > 180) {
+			angle -= 360;
+		}
+		return angle;
 	}
 }
